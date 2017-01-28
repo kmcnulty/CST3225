182b3ed4a5044478d12cb676cf5e7ff93f4489de
kmcnulty <mcnu0067@algonquinlive.com>
2017-01-28 17:03:43 -0500

    Lab 3 with photo upload

--- app/src/main/java/com/example/kathleenmcnulty/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/kathleenmcnulty/androidlabs/ListItemsActivity.java
@@ -4,20 +4,32 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import java.io.File;
+
+import static android.R.attr.data;
 
 public class ListItemsActivity extends AppCompatActivity {
     protected static final String ACTIVITY_NAME = "ListItemsActivity";
      CheckBox checkBox;
     Switch switch1;
+    ImageButton image;
+    final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +37,22 @@ public class ListItemsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_items);
         switch1 = (Switch) findViewById(R.id.switch1);
         checkBox = (CheckBox) findViewById(R.id.checkBox);
+
+
+        image= (ImageButton) findViewById(R.id.imageButton2);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+
+
+
+                }}
+
+        });
 
 
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -107,6 +135,14 @@ public class ListItemsActivity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
         Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            image.setImageBitmap(imageBitmap);
+        }
     }
 
 }
