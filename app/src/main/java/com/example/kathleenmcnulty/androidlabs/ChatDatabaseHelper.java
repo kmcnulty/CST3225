29429fc4499a66ec3bac8ad67ab2be41687c0683
kmcnulty <mcnu0067@algonquinlive.com>
2017-02-18 12:58:36 -0500

    compledted lab 5

---
+++ app/src/main/java/com/example/kathleenmcnulty/androidlabs/ChatDatabaseHelper.java
@@ -0,0 +1,38 @@
+package com.example.kathleenmcnulty.androidlabs;
+
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Kathleen McNulty on 18/02/2017.
+ */
+
+public class ChatDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int VERSION_NUM = 1;
+    public static final String DATABASE_NAME = "MyDatabase";
+    public static final String KEY_ID = "_id";
+    public static final String KEY_MESSAGE = "Key_Message";
+
+    public ChatDatabaseHelper(Context ctx) {
+        super(ctx, DATABASE_NAME, null, VERSION_NUM);
+    }
+    public void onCreate(SQLiteDatabase db) //only called if not yet created
+    {
+        db.execSQL("CREATE TABLE " + DATABASE_NAME+ "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                KEY_MESSAGE+ " VARCHAR(256));");
+        Log.i("ChatDatabaseHelper", "Calling onCreate");
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL("DROP TABLE IF EXISTS " + DATABASE_NAME);
+        onCreate(db);
+        Log.i("ChatDatabaseHelper", "Calling onUpgrade, oldVersion=" + oldVersion + "  newVersion=" + newVersion);
+    }
+
+
+}
--- app/src/main/java/com/example/kathleenmcnulty/androidlabs/chatWindow.java
+++ app/src/main/java/com/example/kathleenmcnulty/androidlabs/chatWindow.java
@@ -1,6 +1,9 @@
 package com.example.kathleenmcnulty.androidlabs;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,28 +27,65 @@ ListView listView;
     EditText editText;
     ArrayList<String> arrayList =new ArrayList<>();
     String position="null";
+    ChatDatabaseHelper dbHelper;
+
     protected static final String ACTIVITY_NAME = "chatWindow";
 
-
+    SQLiteDatabase db;
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        //this refers to the context which is the current activity
+        dbHelper = new ChatDatabaseHelper(this);
+        db = dbHelper.getWritableDatabase();
         setContentView(R.layout.activity_chat_window);
 
         button5=(Button) findViewById(R.id.button4);
         listView=(ListView) findViewById(R.id.listView);
         editText=(EditText) findViewById(R.id.editText4);
         final ChatAdapter messageAdapter =new ChatAdapter( this );
-        listView.setAdapter (messageAdapter);
+        listView.setAdapter(messageAdapter);
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View view) {
-
-             arrayList.add(editText.getText().toString());
+                String msg = editText.getText().toString();
+                //Carries to database and puts it in the right column
+                ContentValues newValues = new ContentValues();
+                //newVakyes is a LINK VARIABLE then matching the content values to the column
+                newValues.put(ChatDatabaseHelper.KEY_MESSAGE, msg);
+            //then im putting it in the database
+            db.insert(ChatDatabaseHelper.DATABASE_NAME, "", newValues);
+             arrayList.add(msg);
                 messageAdapter.notifyDataSetChanged();
                 editText.setText("");
 
             }
         });
+
+
+        //
+        Cursor cursor = db.query(false, ChatDatabaseHelper.DATABASE_NAME,
+                //this means return all values for those fields
+                new String[] { ChatDatabaseHelper.KEY_ID, ChatDatabaseHelper.KEY_MESSAGE},
+                null,null, null, null, null, null);
+        int rows = cursor.getCount() ; //number of rows returned
+        cursor.moveToFirst(); //move to first result
+        Log.i(ACTIVITY_NAME, "Cursor’s  column count =" + cursor.getColumnCount() );
+
+        while(!cursor.isAfterLast() ){
+//
+            Log.i(ACTIVITY_NAME, "SQL MESSAGE:" + cursor.getString( cursor.getColumnIndex( ChatDatabaseHelper.KEY_MESSAGE) ) );
+            //Adds to array
+            arrayList.add(cursor.getString( cursor.getColumnIndex( ChatDatabaseHelper.KEY_MESSAGE) ));
+            //Get's database cursor to move to next element
+            cursor.moveToNext();
+        }
+        //GEt the column names for the log message???
+        for(int i=0;i<cursor.getColumnCount();i++) {
+            cursor.getColumnName(i);
+            Log.i(ACTIVITY_NAME, "Column Name:" +  cursor.getColumnName(i));
+        }
+
 
 
     }
@@ -60,6 +100,7 @@ ListView listView;
             return arrayList.get(position);
         }
         public View getView(int position, View convertView, ViewGroup parent){
+            //Just specifying the chat window is going to use what layout for each item????
             LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
             View result = null ;
             if(position%2 == 0) {
@@ -78,5 +119,14 @@ ListView listView;
 
 
     }
+    public void onDestroy(){
+        super.onDestroy();
+        if(db != null){
+            db.close();
+        }
+        if(dbHelper != null){
+            dbHelper.close();
+        }
 
+    }
 }
